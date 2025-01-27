3dcd35e559d81f59e4dca47c94a8ba803646a51c
Shaheen77-wsu <shaheencollege@yahoo.com>
2025-01-27 12:07:21 -0800

    adding basic menu program + learning git

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// code for the main menu of our game!
+// Shaheen Nafeie
+// CS 320
+//Weekly Progress 1 
+
+using System;
+
+class Menu{
+
+static void Main(){
+
+    bool cougMenu = true;
+
+    while (cougMenu){
+        Console.WriteLine("COUGARS DOMAIN");
+        Console.WriteLine("1
