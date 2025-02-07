be5ca2137b9840cba0acf4253b05212f646278ac
Joseph <leahuem13@gmail.com>
2025-02-06 23:03:11 -0800

    Implemented Main Menu UI with Start and Exit buttons

---
+++ CougarsDomain/.gitignore.cs
@@ -0,0 +1,16 @@
+# Ignore everything in the Library, Logs, and Temp folders
+[Ll]ibrary/
+[Ll]ogs/
+[Tt]emp/
+[Oo]bj/
+[Bb]uild/
+[Bb]uilds/
+[Uu]serSettings/
+
+# Ignore .DS_Store (Mac system files)
+.DS_Store
+
+# Ignore Visual Studio files
+*.csproj
+*.sln
+*.userprefs
