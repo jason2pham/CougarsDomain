61e1df4ee3397b2523bf90762739f3f1832ab94c
Jason Pham <jason2pham@gmail.com>
2025-02-07 21:22:58 -0800

    wsuv map + abilities scipting

---
+++ JasonsCD/Assets/Scripting/Abilities.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card")]
+public class Abilities : SciptableObject 
+{
+
+    public string name;
+    public string description;
+
+    public Sprite artwork;
+
+    public int manaCost;
+    public int damage;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
